c9bc6d38304786daf36554f8290b45d4197af7dc
Muhammed K K <ajumell@xeoscript.com>
2026-08-04 15:55:55 +0530

    Removed JPA query factory bean, as it is already provided by the QueryDSL Utils library.

--- src/main/java/com/xeoscript/modules/customauthtoken/config/CustomAuthTokenAutoConfiguration.java
+++ src/main/java/com/xeoscript/modules/customauthtoken/config/CustomAuthTokenAutoConfiguration.java
@@ -1,22 +1,12 @@
 package com.xeoscript.modules.customauthtoken.config;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
 
 @Configuration
 @ComponentScan(basePackages = "com.xeoscript.modules.customauthtoken")
 @EntityScan(basePackages = {"com.xeoscript.modules.customauthtoken.jpa.entity"})
 public class CustomAuthTokenAutoConfiguration {
 
-    @Bean
-    @ConditionalOnMissingBean
-    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
-        return new JPAQueryFactory(entityManager);
-    }
 }
